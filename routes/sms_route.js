53ced71304c978f50fe55907032ec9d9b7a12797
sanjeev.gehlot <sanjeev.gehlot@mindcrewtech.com>
2018-02-05 12:19:49 +0530

    Validator library

--- routes/sms_route.js
+++ routes/sms_route.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 var sms = require('./../model/sms');
 var KAVENEGAR_API_KEY = require('./../config/kavenegar');
-var phone = require('node-phonenumber')
-
+var validator = require('validator');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -15,13 +14,37 @@ router.post('/send', function(req, res, next) {
 try{
 var reqObj = req.body;  
 
-var phoneUtil = phone.PhoneNumberUtil.getInstance();
-var phoneNumber = phoneUtil.parse(reqObj.phone,'IR');
-var toNumber = phoneUtil.format(phoneNumber, phone.PhoneNumberFormat.INTERNATIONAL);
- 
-//
-var phone_number = toNumber.replace(/ /g,'');
-	console.log(phone_number);
+	if(validator.isMobilePhone(reqObj.phone,'fa-IR') === true){
+		KAVENEGAR_API_KEY.VerifyLookup({
+				receptor: reqObj.phone,
+				token: reqObj.message,
+				template: "registerlogin"
+			}, function(response, status) {
+				console.log(response);
+				console.log(status);
+				  if(status==200){
+					var data = {receiver_number: reqObj.phone, SMS_text:reqObj.message, sent_time:response[0].date, sms_status:"Sent", sender_number:response[0].sender, messageid:response[0].messageid };
+					/*console.log(data);*/
+					sms.create(data, function(err, data){
+						//console.log("created");
+						//console.log(data);
+						
+						res.json({"status":"true","Response":"Message sent Successfully"});
+						
+					})
+				}  
+				else{
+					res.json({"status":"false","Response":"Please try again!"});
+				}
+			}); 
+	}
+	else{
+		
+		res.json({"status":"false","Response":"Please enter correct phone number!"});									
+	}												
+
+
+
 	
 	/* KAVENEGAR_API_KEY.Send({
         message :  reqObj.message,
@@ -35,28 +58,7 @@ var phone_number = toNumber.replace(/ /g,'');
 	 */
 	 
 	 
- 	KAVENEGAR_API_KEY.VerifyLookup({
-		receptor: phone_number,
-		token: reqObj.message,
-		template: "registerlogin"
-	}, function(response, status) {
-		console.log(response);
-		console.log(status);
-		  if(status==200){
-			var data = {receiver_number: phone_number, SMS_text:reqObj.message, sent_time:response[0].date, sms_status:"Sent", sender_number:response[0].sender, messageid:response[0].messageid };
-			/*console.log(data);*/
-			sms.create(data, function(err, data){
-				//console.log("created");
-				//console.log(data);
-				
-				res.json({"status":"true","Response":"Message sent Successfully"});
-				
-			})
-		}  
-		else{
-			res.json({"status":"false","Response":"Please try again!"});
-		}
-	}); 
+ 	
 
 	
 
