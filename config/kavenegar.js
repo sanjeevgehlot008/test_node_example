4c2eaf50e9eedf7ad94d946e67dda1e95d030eb3
sanjeevgehlot008 <31001227+sanjeevgehlot008@users.noreply.github.com>
2018-03-05 14:39:07 +0530

    Update kavenegar.js

--- config/kavenegar.js
+++ config/kavenegar.js
@@ -1,6 +1,6 @@
 var Kavenegar = require('kavenegar');
 var api = Kavenegar.KavenegarApi({
-    apikey: '345271454776727A622B384B53382B323356507233673D3D'
+    apikey: '**********************************'
 });
 
 module.exports = api;
